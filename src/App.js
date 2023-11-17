d2e77a0c945b61ddaad4c117d423ad78d7c7a883
Allison Offer <allison.offer@irdeto.com>
2023-11-17 14:16:49 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { React, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        className="main-content"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "300%",
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: "-15%",
+        }}
+      >
+        <div
+          className="counter"
+          style={{ fontSize: "120%", position: "relative", top: "10vh" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1>{counter}</h1>
+        </div>
+        <div className="buttons">
+          <button
+            onClick={increment}
+            style={{
+              fontSize: "60%",
+              position: "relative",
+              top: "10vh",
+              marginRight: "5px",
+              backgroundColor: "#B1B1B1",
+              borderRadius: "8%",
+              width: 100,
+              color: "white",
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={decrement}
+            style={{
+              fontSize: "60%",
+              position: "relative",
+              top: "10vh",
+              width: 100,
+              marginLeft: "5px",
+              backgroundColor: "#B1B1B1",
+              borderRadius: "8%",
+              color: "white",
+            }}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
